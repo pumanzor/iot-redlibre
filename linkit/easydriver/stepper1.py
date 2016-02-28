d1ee8f2a23abff9b81277774f3ef95ef6a5b0237
Pablo Umanzor A <info@redlibre.cl>
2016-02-28 19:36:26 -0300

    Update stepper1.py

--- linkit/easydriver/stepper1.py
+++ linkit/easydriver/stepper1.py
@@ -1,6 +1,6 @@
 import mraa
-pin = mraa.Pwm(18)
-#pin2 = mraa.Pwm(19)
+#pin = mraa.Pwm(18)
+pin19 = mraa.Pwm(19)
 
 pin1 = mraa.Gpio(1)
 pin1.dir(mraa.DIR_OUT)
@@ -8,6 +8,6 @@ pin1.dir(mraa.DIR_OUT)
 
 while True:
 	pin1.write(0)
-	pin2.period_us(200)      
-	pin2.enable(True)
-	pin2.write(0.1)  
+	pin19.period_us(200)      
+	pin19.enable(True)
+	pin19.write(0.1)  
