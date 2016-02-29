a27358ade63cc9341cbab98e275790a29d23a113
Pablo Umanzor A <info@redlibre.cl>
2016-02-29 15:04:51 -0300

    Update mqtt_easydriver_stepper.py

--- linkit/easydriver/mqtt_easydriver_stepper.py
+++ linkit/easydriver/mqtt_easydriver_stepper.py
@@ -13,12 +13,12 @@ MQTT_PORT = 1883
 
 def on_connect(client, userdata, rc):
     print("\nConnected with result code " + str(rc) + "\n")
-    client.subscribe("/pryxo/yxusers/motor/control/")  
+    client.subscribe("/redlibre/test/motor/control/")  
     print("Subscribed to homecontrol")
 
 def on_message_iotrl(client, userdata, msg):
     print("\n\t* Linkit UPDATED ("+msg.topic+"): " + str(msg.payload))
-    if msg.payload == "m1":
+    if msg.payload == "m2":
 	pin0.write(0)
 	pin1 = mraa.Gpio(1)
 	pin1.dir(mraa.DIR_OUT)
@@ -27,8 +27,8 @@ def on_message_iotrl(client, userdata, msg):
 	pin19.enable(True)
 	pin19.write(0.1)  
 	time.sleep(2) 
-        client.publish("/pryxo/yxusers/iot/status/", "derecha", 2)
-    if msg.payload == "m0":
+        client.publish("/redlibre/test/motor/control/status/", "RIGHT", 2)
+    if msg.payload == "m1":
     	pin0.write(0)
 	pin1 = mraa.Gpio(1)
 	pin1.dir(mraa.DIR_OUT)
@@ -37,17 +37,17 @@ def on_message_iotrl(client, userdata, msg):
 	pin19.enable(True)
 	pin19.write(0.1)  
 	time.sleep(2) 
-        client.publish("/pryxo/yxusers/iot/status/", "izquierda", 2)
-    if msg.payload == "m2": 
+        client.publish("/redlibre/test/motor/control/status/", "LEFT", 2)
+    if msg.payload == "m0": 
 	pin0.write(1)                                                      
-	client.publish("/pryxo/yxusers/iot/status/", "STOP", 2)                                                
+	client.publish("/redlibre/test/motor/control/status", "STOP", 2)                                                
 def command_error():
     print("Error: Unknown command")
 
 client = mqtt.Client(client_id="linkit7688-stepper-motor")
 
 client.on_connect = on_connect
-client.message_callback_add("/pryxo/yxusers/motor/control/", on_message_iotrl)
+client.message_callback_add("/redlibre/test/motor/control/", on_message_iotrl)
 
 client.connect(MQTT_HOST, MQTT_PORT, 60)
 client.loop_start() 
