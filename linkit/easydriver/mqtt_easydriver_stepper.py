fb89be3b2fa368e4fb86e015cfde2cf6c8dbfcf4
Pablo Umanzor A <info@redlibre.cl>
2016-02-29 17:36:43 -0300

    Update mqtt_easydriver_stepper.py

--- linkit/easydriver/mqtt_easydriver_stepper.py
+++ linkit/easydriver/mqtt_easydriver_stepper.py
@@ -3,8 +3,12 @@ import json, time
 import mraa
 
 pin19 = mraa.Pwm(19)
+
 pin0 = mraa.Gpio(0)   
 pin0.dir(mraa.DIR_OUT)
+
+pin1 = mraa.Gpio(1)
+pin1.dir(mraa.DIR_OUT)
 
 # ----- CHANGE THESE FOR YOUR SETUP -----
 MQTT_HOST = "190.97.169.126" #Our free test MQTT server , please use with moderation
@@ -20,8 +24,6 @@ def on_message_iotrl(client, userdata, msg):
     print("\n\t* Linkit UPDATED ("+msg.topic+"): " + str(msg.payload))
     if msg.payload == "m2":
 	pin0.write(0)
-	pin1 = mraa.Gpio(1)
-	pin1.dir(mraa.DIR_OUT)
 	pin1.write(0)
 	pin19.period_us(300)      
 	pin19.enable(True)
@@ -30,8 +32,6 @@ def on_message_iotrl(client, userdata, msg):
         client.publish("/redlibre/test/motor/control/status/", "RIGHT", 2)
     if msg.payload == "m1":
     	pin0.write(0)
-	pin1 = mraa.Gpio(1)
-	pin1.dir(mraa.DIR_OUT)
 	pin1.write(1)
 	pin19.period_us(300)      
 	pin19.enable(True)
